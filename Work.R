0ac85eda67ba2ddc090d70cb5dae8fcebdd4c051
Riu Sakaguchi <sakaguch@grinnell.edu>
2020-06-02 13:53:10 -0700

    more tests

--- Work.R
+++ Work.R
@@ -15,3 +15,33 @@ test3 <- inner_join(test, test2)
 
 test3 <- test3 %>%
   mutate(Percentage = B/Total)
+
+test4 <- data.all %>%
+  group_by(Virus, Medicine) %>%
+  summarize(Destroyed = sum(Destroyed), Missed = sum(Missed)) %>%
+  gather("Destroyed", "Missed", key = "Indicator", value = "ShotDestroyed")
+
+
+total <- test4 %>%
+  group_by(Virus, Medicine) %>%
+  summarize(Total = sum(ShotDestroyed))
+
+test5 <- inner_join(test4, total) %>%
+  mutate(Percent = ShotDestroyed/Total)
+
+
+
+  
+  ggplot(test4, aes(x = Virus, y = ShotDestroyed)) + 
+    geom_bar(stat = "identity", aes(fill = Indicator)) +
+    facet_wrap(. ~ Medicine)
+  
+  
+  test6 <- data.all %>%
+    group_by(Virus) %>%
+    summarize(Destroyed = sum(Destroyed), Missed = sum(Missed))
+  
+  
+  
+  
+  
